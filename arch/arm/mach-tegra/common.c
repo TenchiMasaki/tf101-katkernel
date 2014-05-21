23f8daaca16d4cc9b417d996a0f96cae1ec655bb
Megumi <tenchimasaki@hotmail.com>
2014-05-21 13:14:22 -0600

    Add OC Mode power

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -104,7 +104,11 @@ static struct board_info pmu_board_info;
 static struct board_info display_board_info;
 static struct board_info camera_board_info;
 
+#ifdef CONFIG_KERNEL_OC_MODE
+static int pmu_core_edp = 1350;	/* default 1.2V EDP limit */
+#else
 static int pmu_core_edp = 1200;	/* default 1.2V EDP limit */
+#endif
 static int board_panel_type;
 static enum power_supply_type pow_supply_type = POWER_SUPPLY_TYPE_MAINS;
 
